f8ae26622bebb91ec7ee602d5dd20af9246901d1
Gaurav Jasiwal <jasiwalgaurav@gmail.com>
2022-07-23 06:26:08 +0530

    Conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,27 +1,19 @@
 import { useState } from "react";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
+import ExpenseList from "./ExpenseList";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  const getFilteredItems = (oldItems, filterYear) => {
-    return oldItems.filter(
-      (eachItem) => eachItem.date.getFullYear() == filterYear
-    );
-  };
-
   const [changedYear, setChangedYear] = useState(2022);
 
-  const [expenseItems, setExpenseItems] = useState(
-    getFilteredItems(props.items, changedYear)
+  const expenseItems = props.items.filter(
+    (eachItem) =>
+      eachItem.date.getFullYear().toString() === changedYear.toString()
   );
 
   const yearChangeHandlerForExpenses = (changedYearArg) => {
     setChangedYear(changedYearArg);
-
-    setExpenseItems(getFilteredItems(props.items, changedYearArg));
-    console.log(getFilteredItems(props.items, changedYearArg));
   };
 
   return (
@@ -31,14 +23,7 @@ const Expenses = (props) => {
           selected={changedYear}
           onYearChange={yearChangeHandlerForExpenses}
         />
-        {expenseItems.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        <ExpenseList items={expenseItems} />
       </Card>
     </div>
   );
