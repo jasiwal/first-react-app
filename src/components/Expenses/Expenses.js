5b2baaee736d6be9240eddad7cee97a0e32b8982
Gaurav Jasiwal <jasiwalgaurav@gmail.com>
2022-07-24 14:46:04 +0530

    Fixed build issue

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { React, useState } from "react";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import Card from "../UI/Card";
 import ExpenseList from "./ExpenseList";
