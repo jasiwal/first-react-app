a952da0bf938c23f2a4d5211e188f9b996f56b52
Gaurav Jasiwal <jasiwalgaurav@gmail.com>
2022-07-24 06:59:47 +0530

    Conditional rendering

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,25 +1,40 @@
-import React from "react";
+import { React, useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
-    const saveExpenseDataHandler = (enteredExpenseData) =>{
-        const expenseData = {
-            ...enteredExpenseData,
-            id: Math.random().toString()
-        };
+  const [hideExpenseForm, setHideExpenseForm] = useState(true);
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
 
+    props.onSaveExpense(expenseData);
+    setHideExpenseForm(true);
+    //console.log(expenseData);
+  };
 
-        props.onSaveExpense(expenseData);
+  const onCancelHandler = (event) => {
+    setHideExpenseForm(true);
+  };
 
-        //console.log(expenseData);
-    }
+  const showExpenseFormHandler = (event) => {
+    setHideExpenseForm(false);
+  };
 
-    return (
+  return (
     <div className="new-expense">
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+      {hideExpenseForm ? (
+        <button onClick={showExpenseFormHandler}>Add New Expense</button>
+      ) : (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={onCancelHandler}
+        />
+      )}
     </div>
-    );
-}
+  );
+};
 
 export default NewExpense;
